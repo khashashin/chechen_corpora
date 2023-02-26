0429f7f29cd18cfa4e6dabd660bae5ae1a04e897
Yusuf Khasbulatov <khashashin@gmail.com>
2023-02-26 01:33:55 +0100

    feat(frontend): remove unnecessary console.log output
    This commit removes a console.log statement from the frontend codebase. The console.log statement was used for debugging purposes during development, but is no longer needed and was causing unnecessary noise in the console output.

--- frontend/src/shared/api.tsx
+++ frontend/src/shared/api.tsx
@@ -2,7 +2,5 @@ import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getStats = async () => {
-	const response = await axios.get(`http://127.0.0.1:8000/api/stats`);
-	console.log('getStats response', response);
-	return response;
+	return axios.get(`http://127.0.0.1:8000/api/stats`);
 };
