843930a9c6988444bf7b8b1486b51826e92cccd0
Yusuf Khasbulatov <khashashin@gmail.com>
2023-03-03 03:26:31 +0100

    feat: integrate Sentry logging for error monitoring and reporting.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Global } from '@mantine/core';
+import * as Sentry from '@sentry/browser';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
+
+Sentry.init({
+	dsn: 'https://a995ea29fe37452dbf6644e8270bff92@sentry.khas.dev/7',
+	release: '1.0.0',
+	autoSessionTracking: false,
+});
 
 const queryClient = new QueryClient();
 
