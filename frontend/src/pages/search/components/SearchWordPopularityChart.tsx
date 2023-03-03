25e558cfdf4ffd3bb6b9c14770292cf78878d05e
Yusuf Khasbulatov <khashashin@gmail.com>
2023-03-03 01:44:44 +0100

    feat: display line diagram of word popularity in a specific year using available database resources

--- frontend/src/pages/search/components/SearchWordPopularityChart.tsx
+++ frontend/src/pages/search/components/SearchWordPopularityChart.tsx
@@ -1,21 +1,68 @@
 import { memo, useEffect, useState } from 'react';
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	ResponsiveContainer,
+	CartesianGrid,
+	Tooltip,
+	Brush,
+} from 'recharts';
 
-type SearchWordPopularityChartProps = {
-	searchData: any;
+type ChartDataTypes = {
+	chartData: {
+		[year: string]: string[];
+	} | null;
 };
 
-function SearchWordPopularityChart(props: SearchWordPopularityChartProps) {
-	// TODO: Implement word popularity chart
-	const { searchData } = props;
+function SearchWordPopularityChart(props: ChartDataTypes) {
+	const { chartData } = props;
 
-	const [chartData, setChartData] = useState<any>([]);
+	const [chartDataArray, setChartDataArray] = useState<any>([]);
 
-	useEffect(() => {}, [searchData]);
+	useEffect(() => {
+		if (chartData) {
+			const newChartDataArray = Object.keys(chartData).map((year) => {
+				return {
+					year,
+					words: chartData[year].length,
+				};
+			});
+			setChartDataArray([
+				{
+					year: '1900',
+					words: 0,
+				},
+				...newChartDataArray,
+			]);
+		}
+	}, [chartData]);
 
 	return (
-		<div>
-			<h1>SearchWordPopularityChart</h1>
-		</div>
+		<>
+			<h4>
+				График популярности найденных уникальных слов в текстах по годам (всего найдено{' '}
+				{chartDataArray.reduce((acc: number, item: any) => acc + item.words, 0)} результатов по
+				вашему запросу):
+			</h4>
+			<ResponsiveContainer width='100%' height={220}>
+				<LineChart
+					data={chartDataArray}
+					margin={{
+						top: 5,
+						right: 30,
+						left: 0,
+						bottom: 5,
+					}}>
+					<CartesianGrid strokeDasharray='3 3' />
+					<XAxis dataKey='year' />
+					<YAxis />
+					<Tooltip />
+					<Line type='monotone' dataKey='words' stroke='#2f9e44' strokeWidth={2} />
+				</LineChart>
+			</ResponsiveContainer>
+		</>
 	);
 }
 
--- frontend/src/shared/utils.tsx
+++ frontend/src/shared/utils.tsx
@@ -4,6 +4,29 @@ const SharedUtils = {
 		const v = c === 'x' ? r : (r & 0x3) | 0x8; // eslint-disable-line no-bitwise
 		return v.toString(16);
 	}),
+	getChartData: (data: any) => {
+		return data.results.reduce((acc: any, item: any) => {
+			const year = SharedUtils.getWordUsageYear(item);
+			const words = item.matching_words;
+			if (year) {
+				if (acc[year]) {
+					acc[year] = [...acc[year], ...words];
+				} else {
+					acc[year] = words;
+				}
+			}
+			return acc;
+		}, {});
+	},
+	getWordUsageYear: (data: any) => {
+		const sources = ['book', 'article', 'newspaper', 'other'];
+		return sources.reduce((acc: any, source: string) => {
+			if (data[source] && data[source].year) {
+				return data[source].year;
+			}
+			return acc;
+		}, null);
+	},
 };
 
 export default SharedUtils;
