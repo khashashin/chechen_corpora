e3c7d6daeac1a67d41a93c117c08bdc59db7e6d7
Yusuf Khasbulatov <khashashin@gmail.com>
2023-03-04 02:10:56 +0100

    feat(pwa): add progressive web app (PWA) functionality using Vite PWA plugin
    This commit adds PWA functionality to the application using the Vite PWA plugin, enabling users to use the app offline and improving the app's performance on mobile devices. The changes are scoped to the PWA feature.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+			manifest: {
+				name: 'Chechen Corpora',
+				short_name: 'Chechen Corpora',
+				description: 'Chechen Corpora',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	server: {
 		port: 3000,
 	},
