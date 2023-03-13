21a63f1faa611867c018fe1b8c5031e524a01d5b
Yusuf Khasbulatov <khashashin@gmail.com>
2023-03-14 00:06:24 +0100

    fix(component): fix import path for ArticleMeta component
    The import path for the ArticleMeta component was incorrect and causing errors in the application. This fix corrects the import path so that the component can be used properly.

--- frontend/src/pages/articles/components/ArticleMetaInfo.tsx
+++ frontend/src/pages/articles/components/ArticleMetaInfo.tsx
@@ -1,9 +1,9 @@
-import { ArticleMeta } from '../models';
+import Article from '../../../models/article';
 
 type ArticleMetaInfoProps = {
 	opened: boolean;
-	onClose: (values: ArticleMeta) => void;
-	articleMeta: ArticleMeta;
+	onClose: (values: Article) => void;
+	articleMeta: Article;
 };
 
 function ArticleMetaInfo(props: ArticleMetaInfoProps) {}
